4dd6e36b6b0426cf823ab7a89cd29b9cd29b64ce
Bob.Lichtenfels <Bob.Lichtenfels@BLICHTENFEL6440.corp.tangoe.com>
2016-10-17 13:07:43 -0400

    10/17/16

--- Dash/src/expenses/TotalExpenseByVendorCarrier.java
+++ Dash/src/expenses/TotalExpenseByVendorCarrier.java
@@ -1,23 +1,55 @@
 package expenses;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.openqa.selenium.By;
+import org.testng.Assert;
+
+import com.sun.jna.StringArray;
 
 import Dash.BaseClass;
 
 public class TotalExpenseByVendorCarrier extends BaseClass
 {
-	public static String [] strArray; 
+	public static String [] legendsArray;
+	public static List<String> legendsList = new ArrayList<>();
+	public static int numPieParts = 0;
+	public static int maxNumLegends = 6;	
+	public static String otherString = "Other";
 	
-	public static void getAllLegends()
+	public static void setAllLegendsAndPieCount()
 	{
-		strArray = driver.findElement(By.xpath(".//*[@id='highcharts-0']/*/*[@class='highcharts-legend']")).getText().split("\n");
-		
-		ShowArray(strArray);
-		
-		
-		// .//*[@id='highcharts-0']/*/*[@class='highcharts-legend']
+		// store all legend names.
+		legendsArray= driver.findElement(By.xpath("(//div[@id='highcharts-2']/*/*/*/..)[8]")).getText().split("\n");
+
+		// store number of sections in the pie.
+		numPieParts = driver.findElements(By.xpath(".//*[@id='highcharts-0']/*/*/*[@class='highcharts-series highcharts-series-0 highcharts-tracker']/*")).size();
+
+		// put legend array into list.
+		for (String str : legendsArray)
+		{
+			legendsList.add(str);
+		}
+
 	}
 	
-	
+	public static void VerifyLegendsAndPieCount()
+	{
+		setAllLegendsAndPieCount();
+		System.out.println(numPieParts);
+		
+		if(numPieParts <= 5)
+		{
+			
+		}
+		else
+		{
+			Assert.assertTrue(legendsList.contains(otherString), "");
+			Assert.assertTrue(numPieParts == maxNumLegends, "");
+		}
+		
+		
+	}
 	
 }
--- Dash/src/testSuiteExpense/TotalExpenseByVendorCarrierVisual.java
+++ Dash/src/testSuiteExpense/TotalExpenseByVendorCarrierVisual.java
@@ -31,7 +31,7 @@ public class TotalExpenseByVendorCarrierVisual extends BaseClass
 
 		// The pie shows data only for the five largest vendors selected. Data for other vendors selected are shown under the “Others” slice. 
 		// The “Others” slice should not appear if just five or less vendors are selected.
-		TotalExpenseByVendorCarrier.getAllLegends();
+		TotalExpenseByVendorCarrier.VerifyLegendsAndPieCount();
 		
 		DebugTimeout(9999, "DONE");		
 	}
