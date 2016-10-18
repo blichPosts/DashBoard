986324a52a8adb5497cb870db2e5af6bcad58baa
Bob.Lichtenfels <Bob.Lichtenfels@BLICHTENFEL6440.corp.tangoe.com>
2016-10-18 13:39:48 -0400

    10/18/16

--- Dash/src/testSuiteExpense/TotalExpenseByVendorSpendCategoryVisual.java
+++ Dash/src/testSuiteExpense/TotalExpenseByVendorSpendCategoryVisual.java
@@ -10,9 +10,38 @@ import Dash.BaseClass;
 import expenses.TotalExpenseByVendorCarrier;
 import helperObjects.CommonTestStepActions;
 
-public class TotalExpenseByVendorSpendCategoryVisual 
+public class TotalExpenseByVendorSpendCategoryVisual extends BaseClass
 {
+
+	@BeforeClass
+	public static void setUp() throws Exception
+	{
+		setUpDriver();
+	}
 	
+	@Test
+	public static void TotalExpenseByVendorCarrierVisualTest() throws Exception
+	{
+		CommonTestStepActions.GoToExpensePageDetailedWait();
+		
+		// #1
+		// Select all the vendors in the Point of View and select all of the vendor legends.
+		CommonTestStepActions.SelectAllVendors();
+		
+		
+		
+		
+
+	}
+	
+	@AfterClass
+	public static void closeDriver() throws Exception
+	{
+		System.out.println("Close Browser.");		
+	    JOptionPane.showMessageDialog(frame, "Select OK. This is Ana edit. TEST");
+		driver.close();
+		driver.quit();
+	}	
 	
 
 }
