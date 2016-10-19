8d0aa16a8867dbae827a7730313e77357a7d9457
Ana Laura Pace <analaura.pace@tangoe.com>
2016-10-19 09:36:30 -0300

    Added test for Usage - Vendor View section (not finished yet)

--- Dash/src/helperObjects/UsageHelper.java
+++ Dash/src/helperObjects/UsageHelper.java
@@ -1,6 +1,15 @@
 package helperObjects;
 
-public class UsageHelper {
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+
+import Dash.BaseClass;
+
+public class UsageHelper extends BaseClass{
 
 	// Units of measure for Usage
 	public final static String minutes = "min";
@@ -10,5 +19,111 @@ public class UsageHelper {
 	public final static String dataMB = "MB";
 	public final static String dataGB = "GB";
 	public final static String dataTB = "TB";
+	
+	public final static String domesticTitleVoice = "Domestic (min)"; 
+	public final static String roamingTitleVoice = "Roaming (min)";
+	public final static String domesticTitleDataGB = "Domestic (GB)"; 
+	public final static String roamingTitleDataGB = "Roaming (GB)";
+	public final static String domesticTitleDataTB = "Domestic (TB)"; 
+	public final static String roamingTitleDataTB = "Roaming (TB)";
+	public final static String domesticTitleMessages = "Domestic"; 
+	public final static String roamingTitleMessages = "Roaming";
+	
 
+
+	public static void selectVendorView(){
+		
+		WaitForElementClickable(By.cssSelector("#md-tab-label-1-0"), MediumTimeout, "Vendor View Selector is not clickable.");
+		driver.findElement(By.cssSelector("#md-tab-label-1-0")).click();
+		
+	}
+	
+	
+	public static void verifyMonthYearOnVendorView(){
+		
+		// Get the month and year from Month selector
+		String monthYearSelector = new Select(driver.findElement(By.cssSelector(".tbd-pov__monthPicker>select"))).getFirstSelectedOption().getText();
+		//System.out.println("monthYearSelector:" + monthYearSelector);
+					
+		// Get the month and year from Vendor View
+		String monthYearVendorView = driver.findElement(By.cssSelector(".tdb-h2")).getText();
+		//System.out.println("monthYearVendorView:" + monthYearVendorView);
+					
+		Assert.assertEquals(monthYearSelector, monthYearVendorView, "Month and Year displayed for Total Usage by Vendor is not the same as the selection made under Month selector.");		
+							
+	}
+
+	
+	
+	public static void verifyTotalUsageTitle(){
+		
+		String totalUsageTitleExpected = "Total Usage by Vendor - ";
+		String totalUsageTitleFound = driver.findElement(By.xpath("(//h3[@class='tdb-h3'])[1]")).getText();
+		List<WebElement> categorySelectors = driver.findElements(By.cssSelector(".tdb-inlineBlock.tdb-boxSelector__option"));
+
+		
+		for(int i = 0; i < 3; i++){
+			
+			categorySelectors.get(i).click();
+			totalUsageTitleFound = driver.findElement(By.xpath("(//h3[@class='tdb-h3'])[1]")).getText();
+			
+			//System.out.println("Title found:    " + totalUsageTitleFound);
+			//System.out.println("Title expected: " + totalUsageTitleExpected + categorySelectors.get(i).getText());
+			
+			Assert.assertEquals(totalUsageTitleExpected + categorySelectors.get(i).getText(), totalUsageTitleFound);
+			
+		}
+
+		
+	}
+	
+	
+	public static void verifyBarChartTitlesUsageByVendor(){
+		
+				
+		List<WebElement> barChartTitles;
+		
+		String domesticTitleFound; 
+		String roamingTitleFound;
+		List<WebElement> categorySelectors = driver.findElements(By.cssSelector(".tdb-inlineBlock.tdb-boxSelector__option"));
+
+		
+		for (int i = 0; i < 3; i++){
+			
+			categorySelectors.get(i).click(); 
+			barChartTitles = driver.findElements(By.cssSelector(".tdb-charts__label.tdb-text--italic.tdb-text--bold"));
+			domesticTitleFound = barChartTitles.get(0).getText(); 
+			roamingTitleFound = barChartTitles.get(1).getText();
+			
+			if (i == 0){
+				Assert.assertEquals(domesticTitleFound, domesticTitleVoice);
+				Assert.assertEquals(roamingTitleFound, roamingTitleVoice);
+				System.out.println("Voice...");
+				
+			} else if (i == 1){
+				
+				boolean domesticTitle = domesticTitleFound.equals(domesticTitleDataGB) || domesticTitleFound.equals(domesticTitleDataTB);  
+				Assert.assertTrue(domesticTitle);
+				
+				boolean roamingTitle = roamingTitleFound.equals(roamingTitleDataGB) || roamingTitleFound.equals(roamingTitleDataTB);  
+				Assert.assertTrue(roamingTitle);
+				System.out.println("Data...");
+								
+			} else if (i == 2){
+			
+				Assert.assertEquals(domesticTitleFound, domesticTitleMessages);
+				Assert.assertEquals(roamingTitleFound, roamingTitleMessages);
+				System.out.println("Messages...");
+			
+			}
+			
+		}
+				
+		
+	}
+	
+	
+	
+	
+	
 }
---
+++ Dash/src/testSuiteUsage/TotalUsageByVendor.java
@@ -0,0 +1,66 @@
+package testSuiteUsage;
+
+import javax.swing.JOptionPane;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import Dash.BaseClass;
+import helperObjects.CommonTestStepActions;
+import helperObjects.UsageHelper;
+
+
+
+public class TotalUsageByVendor extends BaseClass {
+		
+		@BeforeClass
+		public static void setUp() throws Exception
+		{
+			setUpDriver();
+		}
+		
+		
+		@Test
+		public static void KpiTilesVisualUsageTest() throws Exception
+		{
+			
+			CommonTestStepActions.GoToUsagePageDetailedWait();
+			
+						
+			// #1 Select Vendor View 
+			UsageHelper.selectVendorView();
+			
+			
+			// #2 Verify Month and Year displayed on Vendor View
+			UsageHelper.verifyMonthYearOnVendorView();
+			
+			
+			// #3 Verify that title "Total Usage by Vendor - ... " changes according to what Category was selected
+			UsageHelper.verifyTotalUsageTitle();
+			
+			
+			// #4 Verify bar charts titles
+			UsageHelper.verifyBarChartTitlesUsageByVendor();
+			
+			
+			// **** CONTINUE HERE *************
+			
+			
+		}
+		
+		
+		@AfterClass
+		public static void closeDriver() throws Exception
+		{
+			System.out.println("Close Browser.");		
+		    JOptionPane.showMessageDialog(frame, "Total Usage by Vendor test finished. Select OK to close browser.");
+			driver.close();
+			driver.quit();
+		}
+
+
+	}
+
+	
+
