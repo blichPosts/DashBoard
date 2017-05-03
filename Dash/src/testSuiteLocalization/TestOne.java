66a2eec2d6d3cb750d89b787f7dd625dd17fece8
Bob.Lichtenfels <Bob.Lichtenfels@BLICHTENFEL6440.corp.tangoe.com>
2017-05-03 06:31:51 -0400

    add wew suites/classes

---
+++ Dash/src/testSuiteLocalization/TestOne.java
@@ -0,0 +1,109 @@
+package testSuiteLocalization;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import Dash.BaseClass;
+import expenses.CostPerServiceNumberTrend;
+import expenses.TotalExpensesTrend;
+import helperObjects.CommonTestStepActions;
+import helperObjects.ExpenseHelper;
+import helperObjects.ExpenseHelper.expenseFilters;
+import localization.ClassOne;
+
+public class TestOne extends BaseClass 
+{
+
+	@BeforeClass
+	public static void setUp() throws Exception
+	{
+		setUpDriver();
+		MainLogin();
+	}
+	
+	@Test
+	public static void TestOneTest() throws Exception
+	{
+		// #1
+		// Select vendors view and view the 'Count of Service Numbers' component.
+
+		// this makes sure the most amount of expense legends are shown. the 'desiredMonth' can be found by running the commented 
+		// code in 'TotalExpenseByVendorCountryActions' under under test suite 'testSuiteExpenseActions'.
+		CommonTestStepActions.selectMonthYearPulldown(ExpenseHelper.desiredMonth);  
+		
+		CommonTestStepActions.GoToExpensePageDetailedWait(); 
+		
+		ClassOne.OpenSeetingsPanel();
+
+		ClassOne.VerifyTextInSettingsPanel();
+		
+		
+		//WaitForElementClickable(By.xpath("(//div[@class='md-sidenav-content']/div/div/button)[2]"), MediumTimeout, "");
+		
+		//driver.findElement(By.xpath("(//div[@class='md-sidenav-content']/div/div/button)[2]")).click();
+		
+		Pause("");
+		
+		// .tdb-slideout__body>div>div:nth-of-type(1) 
+		
+		// (//div[@class='md-sidenav-content']/div/div/button)[2]/span // this select top left columns...
+		
+		// CostPerServiceNumberTrend.SetupChartId(); // setup unique id for this control test.
+		
+		/*
+		// next two lines below use method in 'TotalExpensesTrend' class to test the trend tabs that were added to the 
+		// cost per service number trend control in 17.1
+		ExpenseHelper.SetExpenseFilter(expenseFilters.CostPerServiceNumber); // this indicates which expense filter is being tested.
+		TotalExpensesTrend.VerifyTrendValues();
+		
+		// #1 View the 'Cost per Line Trend for Top 5 Spend' component.
+
+		// The title is 'Cost per Service Number - All Categories'.
+	    // With no vendors selected the control is blank. (NOTE: will be done in actions).
+		CostPerServiceNumberTrend.VerifyTitle(CommonTestStepActions.ExpensesViewMode.vendor);
+
+		// #2 
+		// Select all the vendors. 
+		CommonTestStepActions.SelectAllVendors();
+
+    	// The legends match the legends in the 'Total Expense' control.
+    	// The last 13 months are shown across the bottom of a line graph matching the months in the month pulldown.
+    	// There is a line for each legend (shown in hover).
+		CostPerServiceNumberTrend.VerifyLegends();
+		CostPerServiceNumberTrend.VerifyMonths();
+		CostPerServiceNumberTrend.VerifyNumLegendsMatchNumBarSections();		
+		
+		// this converts the vendor view total expense legends to a list holding what the legends will be in the country view. 
+		// this list is stored in the expense helper class.
+		ExpenseHelper.SetupForCountryPageWait();
+		
+		// #3
+		// Switch to the 'Country View'.
+		// The results are the same as the above steps except countries are shown instead of vendors.
+		CommonTestStepActions.SelectCountryView();
+		
+		// this waits until a legend in the 'count of service numbers' control is found in the list of countries that was stored 
+		// when method 'ExpenseHelper.SetupForCountryViewPageLoad()' was called above.
+		ExpenseHelper.WaitForCountryPageLoad();
+		
+		CostPerServiceNumberTrend.VerifyTitle(CommonTestStepActions.ExpensesViewMode.country);
+		CostPerServiceNumberTrend.VerifyLegends();
+		CostPerServiceNumberTrend.VerifyMonths();
+		CostPerServiceNumberTrend.VerifyNumLegendsMatchNumBarSections();
+		*/
+	}
+	
+	@AfterClass
+	public static void closeDriver() throws Exception
+	{
+		System.out.println("Close Browser.");		
+	    // JOptionPane.showMessageDialog(frame, "Select OK. This is Ana edit. TEST");
+		driver.close();
+		driver.quit();
+	}		
+	
+	
+	
+}
